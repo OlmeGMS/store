0c3aa7d339425c3cccb1fde6520a65a6645f37ac
Olme Marin <olme_gms@hotmail.com>
2018-03-27 20:45:01 -0500

    "subir imagen"

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,8 @@
 'use strict'
-
+var path = require('path');
 var bcrypt = require('bcrypt-nodejs');
 var User = require('../models/user');
+var jwt = require('../services/jwt');
 
 
 function pruebas(req, res) {
@@ -61,9 +62,117 @@ function saveUser(req, res) {
 
 }
 
+function loginUser(req, res) {
+  var params = req.body;
+
+  var email = params.email;
+  var password = params.password;
+
+  User.findOne({
+    email: email.toLowerCase()
+  }, (err, user) => {
+    if (err) {
+      res.status(500).send({
+        message: 'Error en la petición'
+      });
+    } else {
+      if (!user) {
+        res.status(404).send({
+          message: 'El usuario no existe'
+        });
+      } else {
+        // comprobar la contraseña
+        bcrypt.compare(password, user.password, function(err, check) {
+          if (check) {
+            //devolver los datos del ususario logueado
+            if (params.gethash) {
+              // devolver un token de jwt
+              res.status(200).send({
+                token: jwt.createToken(user)
+              });
+            } else {
+              res.status(200).send({
+                user
+              });
+            }
+          } else {
+            res.status(404).send({
+              message: 'El usuario no ha podido loguearse'
+            });
+          }
+        });
+      }
+    }
+  });
+}
+
+function updateUser(req, res) {
+  var userId = req.params.id;
+  var update = req.body;
+
+  User.findByIdAndUpdate(userId, update, (err, userUpdated) => {
+    if (err) {
+      res.status(500).send({
+        message: 'Error al actualizar el usuario'
+      });
+    } else {
+      if (!userUpdated) {
+        res.status(404).send({
+          message: 'No se ha podido actualizar el usuario'
+        });
+      } else {
+        res.status(200).send({
+          user: userUpdated
+        });
+      }
+    }
+  });
+}
+
+function uploadImage(req, res) {
+  var userId = req.params.id;
+  var file_name = 'No subido...';
+
+  if (req.files) {
+    var file_path = req.files.image.path;
+
+    var file_split = file_path.split('\/');
+    var file_name = file_split[2];
+
+    var ext_split = file_name.split('\.');
+    var file_ext = ext_split[1];
+
+    if (file_ext === 'png' || file_ext === 'jpg' || file_ext === 'gif') {
+      User.findByIdAndUpdate(userId, {image: file_name}, (err, userUpdated) => {
+        if (!userUpdated) {
+          res.status(404).send({
+            message: 'No se ha podido actualizar el usuario'
+          });
+        } else {
+          res.status(200).send({
+            user: userUpdated
+          });
+        }
+      });
+    } else {
+      res.status(200).send({
+        message: 'Extensión del archivo no es correcta'
+      });
+    }
+
+    console.log(file_path);
+  } else {
+    res.status(200).send({
+      message: 'No ha subido ninguna imagen ...'
+    });
+  }
+}
 
 
 module.exports = {
   pruebas,
-  saveUser
+  saveUser,
+  loginUser,
+  updateUser,
+  uploadImage
 };
