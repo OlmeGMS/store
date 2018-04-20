7f0aa977962ede1172d7998f31dfbe8dfa700617
Olme Marin <olme_gms@hotmail.com>
2018-04-19 20:06:36 -0500

    "registro"

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -11,14 +11,17 @@ import { User } from './models/user';
 export class AppComponent implements OnInit{
   public title = 'StoreGames';
   public user: User;
+  public user_register: User;
   public identity;
   public token;
   public errorMessage;
+  public alertRegister;
 
   constructor(
     private _userService: UserService
   ) {
     this.user = new User('', '', '', '', '', 'ROLE_USER', '');
+    this.user_register = new User('', '', '', '', '', 'ROLE_USER', '');
   }
 
   ngOnInit(){
@@ -51,8 +54,8 @@ export class AppComponent implements OnInit{
                   }else{
                     // crear elemento en el local storage para tener un token disponible
                     localStorage.setItem('token', token);
-                    console.log(token);
-                    console.log(identity);
+                    // lmpiar el formulario del registro
+                    this.user = new User('', '', '', '', '', 'ROLE_USER', '');
                   }
                   console.log(response);
                 },
@@ -87,5 +90,33 @@ export class AppComponent implements OnInit{
     localStorage.clear();
     this.identity = null;
     this.token = null;
+  }
+
+  onSubmitRegister(){
+    console.log(this.user_register);
+
+    this._userService.register(this.user_register).subscribe(
+      response => {
+        let user = response.user;
+        this.user_register = user;
+
+        if(!user._id){
+          this.alertRegister = 'Error al registrarse';
+        }else{
+          this.alertRegister = 'El registro exitoso, identificate con '+this.user_register.email;
+          //vaciar formulario
+          this.user_register = new User('', '', '', '', '', 'ROLE_USER', '');
+        }
+      },
+      error => {
+        var errorMessage = <any>error;
+
+        if(errorMessage != null){
+          var body = JSON.parse(error._body);
+          this.alertRegister = body.message;
+          console.log(error);
+        }
+      }
+    );
   }
 }
